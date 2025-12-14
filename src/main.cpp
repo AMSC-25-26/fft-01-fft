3a5aab7b4eb5411d05c2d174ec031aa8a9aa65c3
giuliacavoletti <giulia.cavoletti@mail.polimi.it>
2025-12-14 09:36:31 +0000

    test using an analytical function

--- src/main.cpp
+++ src/main.cpp
@@ -56,6 +56,65 @@ int main() {
     std::cout << "The FFT results should match the DFT results (within floating-point error)." << std::endl;
     std::cout << "The IFFT results should match the input signal (within floating-point error)." << std::endl;
 
+    //now we test our fft algorithm with a signal represented by a mathematical function 
+
+    const size_t N2 = 1024;     // Number of samples (must be power of 2)
+    double T_total = 2.0;    // Total duration of the signal (seconds)
+    double dt = T_total / N2; // Sampling interval
+    double a = 5.0;          
+
+
+    
+    // we choose a time domain function: f(t) = e^(-at)
+    auto func = [a](double t) {
+        return std::exp(-a * t);
+    };
+
+    // we know the frequency domain FT: |F(w)| = 1 / sqrt(a^2 + w^2)
+    auto analytical_FT_magnitude = [a](double f) {
+        double omega = 2 * M_PI * f;
+        return 1.0 / std::sqrt(a * a + omega * omega);
+    };
+
+    // sample the function
+    std::vector<std::complex<double>> sampled_signal(N2);
+    for (size_t n = 0; n < N2; n++) {
+        double t = n * dt;
+        sampled_signal[n] = std::complex<double>(func(t), 0.0);
+    }
+
+    // compute fft
+    std::vector<std::complex<double>> fft_result = sampled_signal;
+    FFT::parallel_iterative(fft_result); 
+    
+    
+    // compare results
+    std::cout<<"\n FFT vs Analytical FT Comparison \n"
+              << std::left << std::setw(10) << "Freq(Hz)" 
+              << std::setw(15) << "FFT (Scaled)" 
+              << std::setw(15) << "Analytical FT" 
+              << std::setw(15) << "Error" << "\n";
+    std::cout << "\n";
+
+    for (size_t k = 0; k <= N2 / 2; k++) {
+        // Calculate the frequency
+        double f = (double)k / T_total; 
+
+        // 1. Get FFT Magnitude 
+        double fft_mag = std::abs(fft_result[k]) * dt;
+
+        // 2. Get analytical Magnitude
+        double analytical_mag = analytical_FT_magnitude(f);
+
+        // 3. Compute Error
+        double error = std::abs(fft_mag - analytical_mag);
+
+        std::cout << std::left << std::setw(10) << std::fixed << std::setprecision(2) << f 
+                  << std::setw(15) << std::setprecision(4) << fft_mag 
+                  << std::setw(15) << std::setprecision(4) << analytical_mag 
+                  << std::setw(15) << error << "\n";
+    }
+
     return 0;
 }
  
